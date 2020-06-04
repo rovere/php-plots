795be8de5ddad8844a1a4f54b5720153d21d4bee
Marco Rovere <rovere@users.noreply.github.com>
2020-06-04 17:16:38 +0200

    Add Google Analytics

--- index.php
+++ index.php
@@ -104,7 +104,15 @@ chdir( $target_folder  )
 </script>
 
 <head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-50988891-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'UA-50988891-1');
+</script>
 </head>
 
 <body>
